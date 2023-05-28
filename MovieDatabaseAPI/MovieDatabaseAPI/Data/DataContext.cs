ca8a6a091fbe35ebfd39ff605dfe2d72ee31c57b
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2023-05-28 17:02:29 +0400

    Add Migrations

---
+++ MovieDatabaseAPI/MovieDatabaseAPI/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieDatabaseAPI.Models;
+
+namespace MovieDatabaseAPI.Db
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ MovieDatabaseAPI/MovieDatabaseAPI/Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieDatabaseAPI.Models
+{
+    public enum Statuss
+    {
+        Deleted = 0,
+        Active = 1
+    }
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(200, ErrorMessage = "Name must not exceed 200 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "ShortDescription is required.")]
+        [StringLength(200, ErrorMessage = "ShortDescription must not exceed 200 characters.")]
+        public string ShortDescription { get; set; }
+        [Required(ErrorMessage = "ReleaseDate is required.")]
+
+        public DateTime ReleaseDate { get; set; }
+        [Required(ErrorMessage = "Director is required.")]
+
+        public string Director { get; set; }
+        [Required(ErrorMessage = "Statuss is required.")]
+        public Statuss Statuss { get; set; }
+        [Required(ErrorMessage = "CreateDate is required.")]
+        public DateTime CreateDate { get; set; }
+        
+    }
+}
